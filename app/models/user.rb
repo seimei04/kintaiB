05d0e077ddc34e87930477a35b6901b68ffcc835
yanoseimei <zakzag04@yahoo.co.jp>
2020-01-28 07:51:35 +0000

    Finish add-basic-info

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
   validates :name,  presence: true, length: { maximum: 50 }
+  
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true    
+                    uniqueness: true
+  validates :department, length: { in: 2..50 }, allow_blank: true
+  validates :basic_time, presence: true
+  validates :work_time, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
@@ -17,7 +22,7 @@ class User < ApplicationRecord
     else
       BCrypt::Engine.cost
     end
-    BCrypt::Password.create(string, cost: cost)
+   BCrypt::Password.create(string, cost: cost)
   end
   
   def User.new_token
