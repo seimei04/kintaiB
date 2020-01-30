02e9363f597483e60dd0cb14c0601a5b5715e0f6
yanoseimei <zakzag04@yahoo.co.jp>
2020-01-30 18:25:25 +0000

    Finish edit-attendances

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  get 'signup', to: 'users#new'
+  get '/signup', to: 'users#new'
   
   #ログイン機能
   get '/login', to: 'sessions#new'
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     member do
       get 'edit_basic_info'
       patch 'update_basic_info'
+      get 'attendances/edit_one_month' # この行が追加対象です。
+      patch 'attendances/update_one_month'
     end
     resources :attendances, only: :update
   end
